ff0cd9418cdbda95090b3d7c8a76ddc89c466088
Fernando Jocevine <fernando.6894@ski.sch.id>
2022-05-23 21:03:38 +0700

    Authors and Books Project YT

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+const Author = require('../models/author')
+
+router.get('/', async (req, res) => {
+  let books
+  try {
+    books = await Book.find().sort({ createAt: 'desc' }).limit(10).exec()
+  } catch {
+    books = []
+  }
+  res.render('index', { books: books })
+})
+
+module.exports = router
